7e57fe62227007b12d4b77dd156a12699071bce5
pbarlar <pbarlar@g.educaand.es>
2024-11-28 14:28:25 +0100

    Update Ejercicio3.java

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
-    public static String linea(char caracter, int repeticiones)
+    public static void linea(char caracter, int repeticiones){
 
+    }
+    
     public static void main(String[] args) {
-        
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Ingrese la latura de la figura: ");
+        int altura=entrada.nextInt();
+        System.out.println("Ingresa el caracter que vas a utilizar: ");
+        String caracter=entrada.nextLine();
     }
 }
