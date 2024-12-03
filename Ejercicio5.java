4578c91d8e03740989cb4cfc89ba1f2a979fc914
pbarlar <pbarlar@g.educaand.es>
2024-12-03 09:03:03 +0100

    Update Ejercicio5.java

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
+
 public class Ejercicio5 {
     
-    
+    public static int[] concatena(int[] a,int[] b){
+        int[] ab=new int[a.length+b.length];
+        String cadena="";
+        for (int i = 0; i < a.length; i++) {
+            ab[i]=a[i];
+        }
+        
+        for (int i = a.length; i < ab.length; i++) {
+            ab[i]=b[i-a.length];
+        }
+        return Arrays.asList(ab);
+    }
     
     
     public static void main(String[] args) {
-        
+        int[] a={8,9,0};
+        int[] b={1,2,3};
+        System.out.println(concatena(a, b));
     }
 }
